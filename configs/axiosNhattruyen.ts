981867a193ea145e4c9893dee553341b0db3c42c
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-22 01:25:24 -0500

    Update axiosNhattruyen.ts

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 import queryString from "query-string";
 
-/*const proxy = {
-  host: "la.residential.rayobyte.com",
-  port: 8000,
+const proxy = {
+  host: "http://lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu@la.residential.rayobyte.com:8000",
+  /*port: 8000,
   auth: {
     username: "lindafisher0252_gmail_com",
     password: "k2iaWLJ2xJDAJVu",
-  },
+  },*/
 };
-*/
+
 // sd
 const axiosNhattruyen = axios.create({
   baseURL: "https://nhattruyenplus.com",
-  proxy: "http://lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu@la.residential.rayobyte.com:8000",
+  proxy: proxy,
 });
 
 export default axiosNhattruyen;
