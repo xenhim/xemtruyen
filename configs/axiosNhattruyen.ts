ef3691695daaf009e86ee6450f6e26e52d16881c
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-22 01:37:37 -0500

    Update axiosNhattruyen.ts

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -10,6 +10,30 @@ const proxy = {
   },*/
 };
 
+const proxyList = [
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-2bf09368@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-acf1f149@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-fd544f0a@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-cd717b1f@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-b685baed@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-de629bf1@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-aee4a713@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-baa677b0@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-01b646fa@la.residential.rayobyte.com', port: '8000' },
+  { ip: 'lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu-session-26e7a2b1@la.residential.rayobyte.com', port: '8000' },
+]; // Replace with your own list of proxies
+
+// Function to rotate through the list of proxies
+const rotateProxy = () => {
+  const proxy = proxyList.shift(); // Get the next available proxy
+  proxyList.push(proxy); // Add the current proxy back to the end of the list
+  return {
+    protocol: "http",
+    host: proxy.ip,
+    port: proxy.port,
+  };
+}
+
 // sd
 const axiosNhattruyen = axios.create({
   baseURL: "https://nhattruyenplus.com",
