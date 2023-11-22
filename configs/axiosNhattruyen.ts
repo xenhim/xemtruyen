9b79808715f6a9ce17495ae966a5b046d7fd2582
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-22 02:24:37 -0500

    Update axiosNhattruyen.ts

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import queryString from "query-string";
+import 'axios-proxy-conf';
 
 /*const proxy = {
   host: "http://lindafisher0252_gmail_com:k2iaWLJ2xJDAJVu@la.residential.rayobyte.com:8000",
@@ -9,18 +10,28 @@ import queryString from "query-string";
     password: "k2iaWLJ2xJDAJVu",
   },
 };*/
-    
+const proxyHost = process.env.PROXY_HOST;
+const proxyPort = process.env.PROXY_PORT;
+const proxyUsername = process.env.PROXY_USERNAME;
+
+let proxy: { host: string; port: number; auth?: { username: string; password: string; }; } | undefined;
+
+if (proxyHost && proxyPort) {
+  proxy = {
+    host: proxyHost,
+    port: parseInt(proxyPort),
+  };
+
+  if (proxyUsername) {
+    proxy.auth = {
+      username: proxyUsername,
+    };
+  }
+}
+
 const axiosNhattruyen = axios.create({
   baseURL: "https://nhattruyenplus.com",
-  proxy: {
-    useHttpProxy: true,
-    host: process.env.PROXY_HOST,
-    port: process.env.PROXY_PORT,
-    auth: {
-      username: process.env.PROXY_USERNAME,
-      password: process.env.PROXY_PASSWORD,
-    },
-  },
+  proxy,
 });
 
 export default axiosNhattruyen;
