dbe04631428838ec1e4ddea8e54cd6bbcf073225
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-22 02:48:56 -0500

    Update axiosNhattruyen.ts

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -1,27 +1,58 @@
 import axios from "axios";
 import queryString from "query-string";
-import 'axios-proxy-conf';
 
-const proxyHost = process.env.PROXY_HOST;
-const proxyPort = process.env.PROXY_PORT;
-const proxyUsername = process.env.PROXY_USERNAME;
 
-let proxy: { host: string; port: number; auth: { username: string; password: string; }; } | undefined;
-
-if (proxyHost && proxyPort) {
-  proxy = {
-    host: proxyHost,
-    port: parseInt(proxyPort),
-    auth: {
-      username: proxyUsername || '',
-      password: '',
+ proxies = [
+    {
+      protocol: 'http',
+      host: 'la.residential.rayobyte.com',
+      port: 8000,
+      username: 'lindafisher0252_gmail_com',
+      password: 'k2iaWLJ2xJDAJVu-session-2de46e7d'
     },
-  };
+    {
+      protocol: 'http',
+      host: 'la.residential.rayobyte.com',
+      port: 8000,
+      username: 'lindafisher0252_gmail_com',
+      password: 'k2iaWLJ2xJDAJVu-session-03c2bb79'
+    },
+    {
+      protocol: 'http',
+      host: 'la.residential.rayobyte.com',
+      port: 8000,
+      username: 'lindafisher0252_gmail_com',
+      password: 'k2iaWLJ2xJDAJVu-session-9c8e6fbb'
+    },
+    {
+      protocol: 'http',
+      host: 'la.residential.rayobyte.com',
+      port: 8000,
+      username: 'lindafisher0252_gmail_com',
+      password: 'k2iaWLJ2xJDAJVu-session-7bdf6e47'
+    },
+    {
+      protocol: 'http',
+      host: 'la.residential.rayobyte.com',
+      port: 8000,
+      username: 'lindafisher0252_gmail_com',
+      password: 'k2iaWLJ2xJDAJVu-session-6c3f9207'
+    },
+    {
+      protocol: 'http',
+      host: 'la.residential.rayobyte.com',
+      port: 8000,
+      username: 'lindafisher0252_gmail_com',
+      password: 'k2iaWLJ2xJDAJVu-session-dac75501'
+    }
+  ]
+function get_random_proxy(proxies) {
+  return proxies[Math.floor((Math.random() * proxies.length))];
 }
+
 
 const axiosNhattruyen = axios.create({
   baseURL: "https://nhattruyenplus.com",
-  proxy,
-});
+{ proxy: get_random_proxy(proxies) });
 
 export default axiosNhattruyen;
