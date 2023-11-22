dc493560c23405427a7a9155afdb9e1aa564bbc3
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-21 20:09:34 -0500

    Update axiosNhattruyen.ts

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -2,12 +2,12 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosNhattruyen = axios.create({
-  baseURL: "https://corsproxy.io/?"
+  baseURL: "https://worker-rapid-butterfly-3f3a.xemining.workers.dev"
 });
 
 axiosNhattruyen.interceptors.request.use(
   async (config) => {
-    if (!config.params) return { ...config, url: `?${config.url}` };
+    if (!config.params) return { ...config, url: `?url=${config.url}` };
     const strParams = queryString.stringify(config.params);
     delete config.params;
     const customConfigs = { ...config, url: `?url=${config.url}?${strParams}` };
