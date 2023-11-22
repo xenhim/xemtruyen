34b69d84d7b8e53faa1326f2744876c0f34fef85
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-22 02:53:28 -0500

    Update axiosNhattruyen.ts

--- configs/axiosNhattruyen.ts
+++ configs/axiosNhattruyen.ts
@@ -1,58 +1,60 @@
 import axios from "axios";
 import queryString from "query-string";
 
+const proxies = [
+  {
+    protocol: "http",
+    host: "la.residential.rayobyte.com",
+    port: 8000,
+    username: "lindafisher0252_gmail_com",
+    password: "k2iaWLJ2xJDAJVu-session-2de46e7d",
+  },
+  {
+    protocol: "http",
+    host: "la.residential.rayobyte.com",
+    port: 8000,
+    username: "lindafisher0252_gmail_com",
+    password: "k2iaWLJ2xJDAJVu-session-03c2bb79",
+  },
+  {
+    protocol: "http",
+    host: "la.residential.rayobyte.com",
+    port: 8000,
+    username: "lindafisher0252_gmail_com",
+    password: "k2iaWLJ2xJDAJVu-session-9c8e6fbb",
+  },
+  {
+    protocol: "http",
+    host: "la.residential.rayobyte.com",
+    port: 8000,
+    username: "lindafisher0252_gmail_com",
+    password: "k2iaWLJ2xJDAJVu-session-7bdf6e47",
+  },
+  {
+    protocol: "http",
+    host: "la.residential.rayobyte.com",
+    port: 8000,
+    username: "lindafisher0252_gmail_com",
+    password: "k2iaWLJ2xJDAJVu-session-6c3f9207",
+  },
+  {
+    protocol: "http",
+    host: "la.residential.rayobyte.com",
+    port: 8000,
+    username: "lindafisher0252_gmail_com",
+    password: "k2iaWLJ2xJDAJVu-session-dac75501",
+  },
+];
 
- proxies = [
-    {
-      protocol: 'http',
-      host: 'la.residential.rayobyte.com',
-      port: 8000,
-      username: 'lindafisher0252_gmail_com',
-      password: 'k2iaWLJ2xJDAJVu-session-2de46e7d'
-    },
-    {
-      protocol: 'http',
-      host: 'la.residential.rayobyte.com',
-      port: 8000,
-      username: 'lindafisher0252_gmail_com',
-      password: 'k2iaWLJ2xJDAJVu-session-03c2bb79'
-    },
-    {
-      protocol: 'http',
-      host: 'la.residential.rayobyte.com',
-      port: 8000,
-      username: 'lindafisher0252_gmail_com',
-      password: 'k2iaWLJ2xJDAJVu-session-9c8e6fbb'
-    },
-    {
-      protocol: 'http',
-      host: 'la.residential.rayobyte.com',
-      port: 8000,
-      username: 'lindafisher0252_gmail_com',
-      password: 'k2iaWLJ2xJDAJVu-session-7bdf6e47'
-    },
-    {
-      protocol: 'http',
-      host: 'la.residential.rayobyte.com',
-      port: 8000,
-      username: 'lindafisher0252_gmail_com',
-      password: 'k2iaWLJ2xJDAJVu-session-6c3f9207'
-    },
-    {
-      protocol: 'http',
-      host: 'la.residential.rayobyte.com',
-      port: 8000,
-      username: 'lindafisher0252_gmail_com',
-      password: 'k2iaWLJ2xJDAJVu-session-dac75501'
-    }
-  ]
 function get_random_proxy(proxies) {
   return proxies[Math.floor((Math.random() * proxies.length))];
 }
 
+const proxy = get_random_proxy(proxies); // Pass the proxies array to the function
 
 const axiosNhattruyen = axios.create({
   baseURL: "https://nhattruyenplus.com",
-{ proxy: get_random_proxy(proxies) });
+  proxy: proxy,
+});
 
 export default axiosNhattruyen;
